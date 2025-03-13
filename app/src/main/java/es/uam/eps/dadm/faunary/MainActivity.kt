6c83c7095b7645597defd2cc9f45fbc1bfaee6ee
AlejandroEstevez <alexesmo78@gmail.com>
2025-03-13 19:11:47 +0100

    Unidad 2: botón prueba

--- app/src/main/java/es/uam/eps/dadm/faunary/MainActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/faunary/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val btnTest = findViewById<Button>(R.id.btnTest)
+
+        btnTest.setOnClickListener {
+            textView.text = "Button was clicked"
         }
     }
 }
