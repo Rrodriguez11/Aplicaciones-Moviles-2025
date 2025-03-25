367c9b65f24f610a5d043b676fd42bc9abab5b90
Alejandro Estévez Moyano <alejandro.estevezm@estudiante.uam.es>
2025-03-26 00:14:39 +0100

    Segunda actividad

--- app/src/main/java/es/uam/eps/dadm/faunary/HabitatActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/faunary/HabitatActivity.kt
@@ -1,32 +1,20 @@
 package es.uam.eps.dadm.faunary
 
 import android.os.Bundle
-import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
-import es.uam.eps.dadm.faunary.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class HabitatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        setContentView(R.layout.activity_habitat)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.btnTest.setOnClickListener {
-            val intent = Intent(this, HabitatActivity::class.java)
-            startActivity(intent)
         }
     }
 }
