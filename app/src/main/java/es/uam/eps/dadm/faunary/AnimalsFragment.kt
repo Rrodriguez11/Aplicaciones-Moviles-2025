4ff44d6386c4bac72336aab77b3fdd305e5c208b
AlejandroEstevez <alexesmo78@gmail.com>
2025-05-01 22:15:35 +0200

    Conversión de la sección de animales a un fragmento

---
+++ app/src/main/java/es/uam/eps/dadm/faunary/AnimalsFragment.kt
@@ -0,0 +1,30 @@
+package es.uam.eps.dadm.faunary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import es.uam.eps.dadm.faunary.databinding.FragmentAnimalsBinding
+import es.uam.eps.dadm.faunary.viewmodel.HabitatViewModel
+
+class AnimalsFragment : Fragment() {
+
+    private lateinit var binding: FragmentAnimalsBinding
+    private lateinit var viewModel: HabitatViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_animals, container, false)
+        viewModel = ViewModelProvider(requireActivity())[HabitatViewModel::class.java]
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+}
